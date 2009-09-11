905e519fa71f757f51962d2d8cc2438fe7cf7bd5
Eric Florenzano <floguy@gmail.com>
2009-09-11 15:13:54 -0700

    Make the chat messages show up

--- demos/chat/media/js/chat.js
+++ demos/chat/media/js/chat.js
@@ -2,11 +2,10 @@ $(function() {
 
     function callback(data) {
         var msg = data.raw_data;
-
-
+        $('.items').prepend('<li>' + msg.message + '</li>');
     }
 
-    Hurricane.add_callback('chat', callback);
+    Hurricane.add_callback('comet', callback);
 
     $('#chat-input').submit(function () {
         var data = $('#message').val();
