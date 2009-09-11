bb99fec08acd5aada8b336112c82fe37569ba9e9
Eric Florenzano <floguy@gmail.com>
2009-09-11 07:58:23 -0700

    Making Alex's javascript better :)

--- hurricane/media/js/comet.js
+++ hurricane/media/js/comet.js
@@ -1,14 +1,16 @@
 $(function() {
-    function ajax_request() {
+    var ajax_request = function() {
         $.ajax({url: '/comet/', type: 'POST', dataType: 'text', success: on_success, error: on_error});
-    }
+    };
     
     var on_success = function(response) {
         $('body').append(response);
         ajax_request();
     };
     var on_error = function(response) {
+        alert(response);
         ajax_request();
     };
-    ajax_request()
+    
+    ajax_request();
 });
