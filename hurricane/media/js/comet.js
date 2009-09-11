6c34c0b38515bb6b02f8f455577f5abaa2f1a22a
Eric Florenzano <floguy@gmail.com>
2009-09-11 07:59:48 -0700

    Yak shaving

--- hurricane/media/js/comet.js
+++ hurricane/media/js/comet.js
@@ -1,14 +1,15 @@
 $(function() {
-    var ajax_request = function() {
-        $.ajax({url: '/comet/', type: 'POST', dataType: 'text', success: on_success, error: on_error});
-    };
-    
     var on_success = function(response) {
         $('body').append(response);
         ajax_request();
     };
+    
     var on_error = function(response) {
         ajax_request();
+    };
+    
+    var ajax_request = function() {
+        $.ajax({url: '/comet/', type: 'POST', dataType: 'text', success: on_success, error: on_error});
     };
     
     ajax_request();
