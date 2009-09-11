68afd86840d292a9089070c65ca8cb393c6892b2
Eric Florenzano <floguy@gmail.com>
2009-09-11 04:36:12 -0700

    Change the runner to use our new convention

--- hurricane/runner.py
+++ hurricane/runner.py
@@ -19,14 +19,14 @@ class ApplicationManager(object):
         self.producer_queue = multiprocessing.Queue()
         
         for producer in settings.PRODUCERS:
-            ProducerClass = import_module(producer)
+            ProducerClass = import_module(producer + '.Producer')
             producer = ProducerClass(settings, self.producer_queue)
             multiprocessing.Process(target=producer.run).start()
         
         self.receiver_queues = []
 
         for consumer in settings.CONSUMERS:
-            ConsumerClass = import_module(consumer)
+            ConsumerClass = import_module(consumer + '.Consumer')
             recv_queue = multiprocessing.Queue()
             consumer = ConsumerClass(settings, recv_quee)
             self.receiever_queues.append(recv_queue)
