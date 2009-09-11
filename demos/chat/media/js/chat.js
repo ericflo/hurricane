8de4526a424e94beda9e1572cd7c33724fbe9912
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-11 14:24:18 -0700

    Fix stuff

--- demos/chat/media/js/chat.js
+++ demos/chat/media/js/chat.js
@@ -2,9 +2,16 @@ $(function() {
 
     function callback(data) {
         var msg = data.raw_data;
-        
-        
+
+
     }
 
     Hurricane.add_callback('chat', callback);
+
+    $('#chat-input').submit(function () {
+        var data = $('#message').value()
+        $.ajax({
+
+        })
+    });
 });
