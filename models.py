1c7d5427a49f8de37c9f4eb16984ed119d47defe
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-11 03:19:51 -0700

    Added a models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from django.db import models
+from django.utils import simplejson
+
+class Item(models.Model):
+    type = models.CharField(max_length=20)
+    timestamp = models.DateTimeField(default=datetime.now)
+    raw_data = models.TextField()
+    
+    @property
+    def data(self):
+        return simplejson.loads(self.raw_data)
