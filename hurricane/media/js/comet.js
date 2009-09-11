2d3cb4fa63b8c64b1e30ba56f5f8feb21af68e3c
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-11 10:56:45 -0700

    ericflo fail

--- hurricane/media/js/comet.js
+++ hurricane/media/js/comet.js
@@ -1,16 +1,16 @@
 var Hurricane = (function() {
     Hurricane = function() {
         var hurricane = this;
-        
+
         this.cursor = null;
         this.initial_backoff = 500;
         this.backoff_iteration = 1;
         this.url = '/comet/';
         setTimeout(function() {
-            hurricane.ajax_request(); 
+            hurricane.ajax_request();
         }, 50); /* Probably should have a better way to start the first request */
     };
-    
+
     Hurricane.prototype = {
         on_success: function(response) {
             var messages = JSON.parse(response).messages;
@@ -20,7 +20,7 @@ var Hurricane = (function() {
                 var msg = messages[msg_idx];
                 var json_msg = JSON.stringify(msg);
                 $('.items').prepend('<li>' + json_msg + '</li>');
-                
+
                 if ($('.items li').length > 200) {
                     $('.items li:last').remove();
                 }
@@ -28,18 +28,18 @@ var Hurricane = (function() {
             this.backoff_iteration = 1;
             this.ajax_request();
         },
-        
+
         on_error: function(response) {
             setTimeout(function() {
-                hurricane.ajax_request(); 
+                hurricane.ajax_request();
             }, this.backoff_iteration * this.initial_backoff);
         },
-        
+
         ajax_request: function() {
             var args = $.param({'cursor': this.cursor});
             $.ajax({url: this.url, type: 'POST', dataType: 'text', data: args, success: this.on_success, error: this.on_error});
-        },
+        }
     }
-    
+
     return new Hurricane();
 })();
