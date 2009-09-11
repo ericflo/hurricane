9a8f0f19e8845a4f3f4bcdf4858a2531bee0df00
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-11 15:08:38 -0700

    fix

--- demos/chat/media/js/chat.js
+++ demos/chat/media/js/chat.js
@@ -13,13 +13,8 @@ $(function() {
         $.ajax({
             url: '/comet/',
             method: 'POST',
-<<<<<<< HEAD
             data: JSON.stringify({'message': data})
-        })
-=======
-            data: data
         });
         return false;
->>>>>>> f5b7e4cdb430a29734c71d6725a355ec97fe7bed
     });
 });
