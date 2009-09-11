22872d24a1d546b94975288f4738e1e0d5e68e4c
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-11 04:10:20 -0700

    Moved to a right proper architecture for runner.py

--- hurricane/runner.py
+++ hurricane/runner.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
 
+import multiprocessing
 import optparse
 
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.importlib import import_module
 
-def main():
-    parser = optparse.OptionParser()
-    parser.add_option('--settings', dest='settings')
-    
-    options, args = parser.parse_args()
-    if not options.settings:
-        raise ImproperlyConfigured("You didn't provide a settings module.")
-    settings = import_module(options.settings)
-    
-    for consumer in settings.CONSUMERS:
-        ConsumerClass = import_module(consumer)
-        consumer = ConsumerClass(settings)
-        consumer.run()
-    
-    for producer in settings.PRODUCERS:
-        ProducerClass = import_module(producer)
-        producer = ProducerClass(settings)
-        producer.run()
-    
+class ApplicationManager(object):
+    def run(self):
+        parser = optparse.OptionParser()
+        parser.add_option('--settings', dest='settings')
+        
+        options, args = parser.parse_args()
+        if not options.settings:
+            raise ImproperlyConfigured("You didn't provide a settings module.")
+        settings = import_module(options.settings)
+        
+        self.producer_queue = multiprocessing.Queue()
+        
+        for producer in settings.PRODUCERS:
+            ProducerClass = import_module(producer)
+            producer = ProducerClass(settings, self.producer_queue)
+            producer.run()
+        
+        self.receiver_queues = []
+
+        for consumer in settings.CONSUMERS:
+            ConsumerClass = import_module(consumer)
+            recv_queue = multiprocessing.Queue()
+            consumer = ConsumerClass(settings, recv_quee)
+            self.receiever_queues.append(recv_queue)
+            consumer.run()
+        
+        while True:
+            item = self.producer_queue.get()
+            for recv_queue in self.receiver_queues:
+                recv_queue.put(item)
+
 
 if __name__ == '__main__':
-    main()
+    app = ApplicationManager()
+    app.run()
