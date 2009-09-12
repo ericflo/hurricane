237e71171e51fc462ce24ef890454f86db6dffbf
Eric Florenzano <floguy@gmail.com>
2009-09-12 13:31:05 -0700

    Added more detail to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,16 @@ from setuptools import setup
 setup(
     name="hurricane",
     version="0.1",
+    description="Hurricane is a project for easily creating Comet web applications",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Environment :: Web Environment",
+    ],
+    keywords="comet,asynchronous,messages,messaging,framework",
     packages = ["hurricane", "hurricane.handlers", "hurricane.managers"],
-    author="The Awesum",
+    author="The Hurricane Development Team",
+    license='BSD',
     url="http://github.com/ericflo/hurricane",
-    description="Hurricane is teh awesome"
 )
