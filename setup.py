994b6d9f69a9e04c6efb678f333d94f0effcd4cc
Armin Ronacher <armin.ronacher@active-4.com>
2009-09-12 11:56:46 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="hurricane",
+    version="0.1",
+    packages = ["hurricane", "hurricane.handlers", "hurricane.managers"],
+    author="The Awesum",
+    url="http://github.com/ericflo/hurricane",
+    description="Hurricane is teh awesome"
+)
