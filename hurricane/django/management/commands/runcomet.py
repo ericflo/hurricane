bdf6e7cd6a75461da2fd2fd5f8f3b1ff588ffab5
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-13 10:18:27 -0700

    No reason for import in function

--- hurricane/django/management/commands/runcomet.py
+++ hurricane/django/management/commands/runcomet.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
+from hurricane.runner import main
+
+
 class Command(BaseCommand):
     help = "Starts a lightweight Comet server for development."
 
     def handle(self, *args, **options):
-        from hurricane.runner import main
         main(settings)
