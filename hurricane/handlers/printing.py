972fab38df919531f4e04d0cd91128df3575f12a
Eric Florenzano <floguy@gmail.com>
2009-09-19 15:47:19 -0700

    Pretty print output

--- hurricane/handlers/printing.py
+++ hurricane/handlers/printing.py
@@ -1,7 +1,8 @@
 from hurricane.handlers.base import PureConsumeHandler
 
+from pprint import pprint
 
 class PrintingHandler(PureConsumeHandler):
 
     def receive(self, msg):
-        print msg
+        print pprint(msg._asdict())
