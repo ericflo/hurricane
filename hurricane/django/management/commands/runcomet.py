0a3b8dc334632211efa1551c40dfda06b77c15e4
Eric Florenzano <floguy@gmail.com>
2009-09-19 20:39:50 -0700

    Fixed the runcomet script

--- hurricane/django/management/commands/runcomet.py
+++ hurricane/django/management/commands/runcomet.py
@@ -1,6 +1,7 @@
 import os
 from subprocess import Popen
 import sys
+from multiprocessing import Process
 
 from twisted.internet import reactor
 from twisted.web import proxy, server
@@ -8,6 +9,14 @@ from twisted.web import proxy, server
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
+from hurricane.runner import main
+
+def start_proxy():
+    main_site = proxy.ReverseProxyResource('localhost', 8000, '')
+    main_site.putChild('comet/',
+        proxy.ReverseProxyResource('localhost', 8001, 'comet/'))
+    reactor.listenTCP(8080, server.Site(main_site))
+    reactor.run()
 
 class Command(BaseCommand):
     help = ("Starts a lightweight Comet server, and django development server "
@@ -20,15 +29,12 @@ class Command(BaseCommand):
             'runserver', 
             '--settings=%s' % settings.SETTINGS_MODULE
         ])
-        Popen([
-            sys.executable, 
-            os.path.abspath(os.path.join(os.path.dirname(__file__),
-                '..', '..', '..', 'runner.py')),
-            '--settings=%s' % settings.SETTINGS_MODULE
-        ])
+        Process(target=start_proxy).start()
+        #Popen([
+        #    sys.executable, 
+        #    os.path.abspath(os.path.join(os.path.dirname(__file__),
+        #        '..', '..', '..', 'runner.py')),
+        #    '--settings=%s' % settings.SETTINGS_MODULE
+        #])
         
-        main_site = proxy.ReverseProxyResource('localhost', 8000, '')
-        main_site.putChild('comet/',
-            proxy.ReverseProxyResource('localhost', 8001, 'comet/'))
-        reactor.listenTCP(8080, server.Site(main_site))
-        reactor.run()
+        main(settings)
