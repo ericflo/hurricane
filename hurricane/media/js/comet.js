dfeb3e2dfffbc76c062464e3608f9ce6b5f3d6d9
Eric Florenzano <floguy@gmail.com>
2009-09-19 23:55:20 -0700

    Use window.BEGINNING_CURSOR

--- hurricane/media/js/comet.js
+++ hurricane/media/js/comet.js
@@ -1,6 +1,6 @@
 var Hurricane = (function() {
     Hurricane = function() {        
-        this.cursor = BEGINNING_CURSOR || null;
+        this.cursor = window.BEGINNING_CURSOR || null;
         this.backoff = 500;
         this.url = '/comet/';
         this.ajax_request();
