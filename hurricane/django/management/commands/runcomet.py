ce1c4866195d4a83de39016c698ae412bb3b2c7b
Eric Florenzano <floguy@gmail.com>
2009-09-19 21:45:37 -0700

    Added back the runcomet because it's convenient

---
+++ hurricane/django/management/commands/runcomet.py
@@ -0,0 +1,41 @@
+import os
+from subprocess import Popen
+import sys
+from multiprocessing import Process
+ 
+from twisted.internet import reactor
+from twisted.web import proxy, server
+from twisted.web.resource import Resource
+ 
+from django.core.management.base import BaseCommand
+from django.conf import settings
+ 
+from hurricane.runner import main
+
+class ProxyResource(Resource):
+    def getChild(self, path, request):
+        if path.startswith('comet'):
+            return proxy.ReverseProxyResource('localhost', 8001, '/comet')
+        elif path.startswith('global'):
+            return proxy.ReverseProxyResource('localhost', 8001, '/global')
+        return proxy.ReverseProxyResource('localhost', 8000, '/' + path)
+
+def start_proxy():
+    root = ProxyResource()
+    reactor.listenTCP(8080, server.Site(root))
+    reactor.run()
+ 
+class Command(BaseCommand):
+    help = ("Starts a lightweight Comet server, and django development server "
+        "for development.")
+ 
+    def handle(self, *args, **options):
+        Popen([
+            sys.executable,
+            sys.argv[0],
+            'runserver',
+            '--settings=%s' % settings.SETTINGS_MODULE
+        ])
+        Process(target=start_proxy).start()
+        
+        main(settings)
